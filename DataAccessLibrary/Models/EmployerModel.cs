e4dc250a076059cb5e86659fce48b53b6c172243
Pierre Plourde <pierre@spartancsharp.net>
2024-02-20 09:58:01 -0500

    Add Visual Studio Project Templates
    C# Console Application and C# Class Library with code from MongoDB Homework.

---
+++ DataAccessLibrary/Models/EmployerModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace DataAccessLibrary.Models
+{
+	public class EmployerModel
+	{
+		[BsonId]
+		public Guid Id { get; set; } = Guid.NewGuid();
+		public string CompanyName { get; set; }
+		public List<AddressModel> Addresses { get; set; } = new List<AddressModel>();
+	}
+}
